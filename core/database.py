f89d4bf2d827da2f1484a9ec89123dc7c8237b8e
misha <medinskijmisa228@gmail.com>
2024-11-12 17:47:56 +0200

    add database controller

---
+++ core/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from .config import config
+
+class DBsettings():
+    def __init__(db_url: str, echo: bool = False):
+        self.engine = create_async_engine(
+            echo=echo, url=url
+        )
+        self.session_factory = async_sessionmaker(
+            bind=self.engine, # привязка движка к фабрики сессий
+            expire_on_commit=False, # кеширование обьектов в сессии
+            autoflush=False, # отключение произвольных действий со стороны ORM
+            autocommit=False # отключение автоматических коммитов
+        )
+    
+    def session(self) -> AsyncSession:
+        with self.session_factory() as session:
+            yield session
+            await session.close()
