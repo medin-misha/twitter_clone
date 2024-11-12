d7482d0edbca5675abdd463ec3e41e427cbc8d37
misha <medinskijmisa228@gmail.com>
2024-11-12 12:13:54 +0200

    add frontend

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    with open("static/index.html", "r", encoding="utf-8") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content)
+
+
+@app.get("/api/users/me")
+async def func():
+    return {
+        "result": "true",
+        "user": {
+            "id": "int",
+            "name": "str",
+            "followers": [{"id": "int", "name": "str"}],
+            "following": [{"id": "int", "name": "str"}],
+        },
+    }
+
+
+app.mount("/static", StaticFiles(directory="static/"), name="static")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
