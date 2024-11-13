fa006491a324488037d6a7ccd4776a7140f7f7ef
misha <medinskijmisa228@gmail.com>
2024-11-13 17:34:37 +0200

    refactor and fix database.py errors

--- core/database.py
+++ core/database.py
@@ -1,19 +1,18 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from .config import config
 
-class DBsettings():
-    def __init__(db_url: str, echo: bool = False):
-        self.engine = create_async_engine(
-            echo=echo, url=url
-        )
+
+class DBsettings:
+    def __init__(self, db_url: str, echo: bool = False):
+        self.engine = create_async_engine(echo=echo, url=db_url)
         self.session_factory = async_sessionmaker(
-            bind=self.engine, # привязка движка к фабрики сессий
-            expire_on_commit=False, # кеширование обьектов в сессии
-            autoflush=False, # отключение произвольных действий со стороны ORM
-            autocommit=False # отключение автоматических коммитов
+            bind=self.engine,  # привязка движка к фабрики сессий
+            expire_on_commit=False,  # кеширование обьектов в сессии
+            autoflush=False,  # отключение произвольных действий со стороны ORM
+            autocommit=False,  # отключение автоматических коммитов
         )
-    
-    def session(self) -> AsyncSession:
+
+    async def session(self) -> AsyncSession:
         with self.session_factory() as session:
             yield session
             await session.close()
