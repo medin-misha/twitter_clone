ec15980e8b0021495c6a42e0c783882660cc544a
misha <medinskijmisa228@gmail.com>
2024-11-13 17:37:01 +0200

    remove fallowing_id and fallowers_ids and add fallowing_ids and fallowers_ids relationships

--- core/models/user.py
+++ core/models/user.py
@@ -1,15 +1,28 @@
 """
 файл для модели user
 """
+
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey, String
 from typing import List
-from base import Base
+from .base import Base
 
 
 class User(Base):
     name: Mapped[str] = mapped_column(String(100), unique=True)
 
-    following_ids: Mapped[List[int]] = mapped_column(ForeignKey("userfallow.autor_id"))
-    followers_ids: Mapped[List[int]] = mapped_column(ForeignKey("userfallow.follower_id"))
-    liked_tweets: Mapped[List["Tweet"]] = relationship(back_populates="liked_users", uselist=True, secondary="userliketweet")
+    following_ids: Mapped[List[int]] = relationship(
+        back_populates="autor_id",
+        secondary="userfollow",
+        secondaryjoin="user.id == userfollow.autor_id",
+    )
+    followers_id: Mapped[List[int]] = relationship(
+        back_populates="autor_id",
+        secondary="userfollow",
+        secondaryjoin="user.id == userfollow.follower_id",
+    )
+
+    # followers_ids: Mapped[List[int]] = mapped_column(ForeignKey("userfallow.follower_id"))
+    liked_tweets: Mapped[List["Tweet"]] = relationship(
+        back_populates="liked_users", uselist=True, secondary="userliketweet"
+    )
