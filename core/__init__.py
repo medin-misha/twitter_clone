af8529b5457f7caab5a4565fb036a931337667ed
misha <medinskijmisa228@gmail.com>
2024-11-13 17:32:40 +0200

    add models in __init__

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,8 @@
-__all__ = "Base",
+__all__ = ("Base",)
 
 from .models.base import Base
+
+from .models.image import Image
+from .models.user import User
+from .models.tweet import Tweet
+from .models.table_communications import UserFallow, UserLikeTweet
