df358aaf04dca0b8af16d1836d67a4f7b28b3eb9
misha <medinskijmisa228@gmail.com>
2024-11-14 18:44:02 +0200

    make response-maker functions

--- handlers/utils/responses.py
+++ handlers/utils/responses.py
@@ -1,22 +1,18 @@
 from typing import Any, Dict
 
 
-def error(msg: str, err_type: str) -> dict[str, bool | str]:
+def error_response(msg: str, err_type: str) -> dict[str, bool | str]:
     """
     Функция принимающая которая возвращает сообщение об ошибке:
         msg в качестве сообщения
         err_type в качестве типа ошибки (statuscode)
     Возвращает понятный для фронтенда dict который будет выводиться конечному пользователю.
     """
-    response = {
-        "result": False,
-        "error_message": f"{msg}",
-        "error_type": f"{err_type}"
-    }
+    response = {"result": False, "error_message": f"{msg}", "error_type": f"{err_type}"}
     return response
 
 
-def frontend_response(resp: dict[Any]) -> dict[bool, Any]:
+def ok_response(resp: dict[Any]) -> dict[bool, Any]:
     """
     Функция задача которой возвращать ответы фронтенду.
     Пока что она очень простая и просто добавляет к ответу result = True.
