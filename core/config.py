cc93db800f19dfda56f6db0a3a53c83866ba71f2
misha <medinskijmisa228@gmail.com>
2024-11-14 18:48:19 +0200

    add image_saver_url and update database path

--- core/config.py
+++ core/config.py
@@ -2,7 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_url: str = "postgresql+asyncpg://postgres:postgres@localhost"
+    db_url: str = "postgresql+asyncpg://postgres_user:postgres_password@localhost:5430/postgres_db"
+    image_saver_url: str = "http://localhost:8000"
 
 
 config = Settings()
