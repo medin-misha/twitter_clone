11e8a163c30d41b423e48e946ef7ce679281d91f
misha <medinskijmisa228@gmail.com>
2024-11-14 18:39:50 +0200

    add config db_settings

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,10 @@
-__all__ = ("Base",)
+__all__ = ("Base", "Image", "User", "Tweet", "config", "db_settings")
+
+
+from .config import config
+from .database import db_settings
 
 from .models.base import Base
-
 from .models.image import Image
 from .models.user import User
 from .models.tweet import Tweet
