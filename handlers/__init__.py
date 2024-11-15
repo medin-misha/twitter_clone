6778c940be6fc3046ccb4a1463cb3326d595f0d4
misha <medinskijmisa228@gmail.com>
2024-11-15 17:46:07 +0200

    refactor init

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,7 @@
-__all__ = "error", "ok_response", "create", "delete", "user_router"
+__all__ = "error", "ok_response", "create", "delete", "user_router", "get_list"
 
 
 from .utils.responses import error_response, ok_response
-from .utils.db_actions import create, delete
+from .utils.db_actions import create, remove, get_list, get_by_id
 
 from .users.views import router as user_router
