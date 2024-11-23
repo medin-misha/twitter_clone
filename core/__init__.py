5a58340bd189706369a6f32c5f120315feb64216
misha <medinskijmisa228@gmail.com>
2024-11-23 13:06:01 +0200

    __init__

--- core/__init__.py
+++ core/__init__.py
@@ -4,8 +4,9 @@ __all__ = ("Base", "Image", "User", "Tweet", "config", "db_settings")
 from .config import config
 from .database import db_settings
 
-from .models.base import Base
-from .models.image import Image
+
 from .models.user import User
 from .models.tweet import Tweet
+from .models.image import Image
 from .models.table_communications import UserFallow, UserLikeTweet
+from .models.base import Base
