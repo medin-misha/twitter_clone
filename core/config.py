f8faa215e7f04c159fc1130ee0ff4c277a992bc7
misha <medinskijmisa228@gmail.com>
2024-11-23 13:06:58 +0200

    update Settings class

--- core/config.py
+++ core/config.py
@@ -2,8 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_url: str = "postgresql+asyncpg://postgres_user:postgres_password@localhost:5430/postgres_db"
-    image_saver_url: str = "http://localhost:8000"
+    db_url: str = (
+        "postgresql+asyncpg://postgres_user:postgres_password@localhost:5430/postgres_db"
+    )
+    image_saver_url: str = "http://127.0.0.1:8000/"
 
 
 config = Settings()
