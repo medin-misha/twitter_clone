9967bc93b6b6a447f06e1039e06aec33d07b7da6
misha <medinskijmisa228@gmail.com>
2024-11-23 13:01:56 +0200

    add get image view

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
-from handlers import user_router
+from handlers import user_router, media_router, tweet_router, get_image
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(media_router)
+app.include_router(tweet_router)
 
 
 @app.get("/me", response_class=HTMLResponse)
-async def read_root():
-    with open("static/index.html", "r", encoding="utf-8") as f:
-        html_content = f.read()
-    return HTMLResponse(content=html_content)
+async def main():
+    return FileResponse("./static/index.html")
 
 
-app.mount("/", StaticFiles(directory="static/"), name="static")
+app.mount("/static", StaticFiles(directory="./static/"), name="static")
+
+
+@app.get("/{filename}")
+async def get_image_view(filename: str):
+    image = await get_image(filename=filename)
+    return image
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8977)
