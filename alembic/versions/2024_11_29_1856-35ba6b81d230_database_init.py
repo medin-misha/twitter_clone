aaa8f24df003534390bf171e02430c2c0d65d148
misha <medinskijmisa228@gmail.com>
2024-11-29 18:57:22 +0200

    models

--- alembic/versions/2024_11_29_1856-35ba6b81d230_database_init.py
+++ alembic/versions/2024_11_29_1856-35ba6b81d230_database_init.py
@@ -1,8 +1,8 @@
-"""models init
+"""database init
 
-Revision ID: 6073330721e1
+Revision ID: 35ba6b81d230
 Revises: 
-Create Date: 2024-11-21 10:53:54.563676
+Create Date: 2024-11-29 18:56:39.962368
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '6073330721e1'
+revision: str = '35ba6b81d230'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -31,7 +31,7 @@ def upgrade() -> None:
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('content', sa.String(), nullable=False),
     sa.Column('user_id', sa.Integer(), nullable=False),
-    sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
+    sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('usersfallows',
@@ -46,7 +46,7 @@ def upgrade() -> None:
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('filename', sa.String(), nullable=False),
     sa.Column('tweet_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['tweet_id'], ['tweets.id'], ),
+    sa.ForeignKeyConstraint(['tweet_id'], ['tweets.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('filename')
     )
